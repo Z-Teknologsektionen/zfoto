fd49888eb4b4e704d324842bf08c1c3cd1dc5a6e
Dennis Holmström <dennis@holmstrom.nu>
2022-12-17 17:01:38 +0100

    trying new refer policy when fetching images

--- src/components/albumGrid/AlbumGridItem.tsx
+++ src/components/albumGrid/AlbumGridItem.tsx
@@ -23,12 +23,13 @@ export const AlbumGridItem: FC<{
         `}
         priority={priorityLoadning}
         quality={100}
+        referrerPolicy="no-referrer"
         sizes="400px"
         src={
           filename ? `http://holmstrom.ddns.net:8080/df/thumb/${filename}` : ""
         }
         fill
-        /* unoptimized */
+        unoptimized
       />
       <div className="z-10 w-full bg-[#333333]/95 py-3 px-4 text-[#a7a7a7]">
         <p className="truncate text-center font-semibold">{title}</p>
