b92f15bb0f0f08c605adea5b825611612fe03552
Dennis Holmström <dennis@holmstrom.nu>
2023-02-14 14:01:25 +0100

    Fix: For devices that dont suport aspect ratio

--- src/components/imageGrid/ImageGridItem.tsx
+++ src/components/imageGrid/ImageGridItem.tsx
@@ -10,7 +10,7 @@ export const ImageGridItem: FC<{
   return (
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
     <div
-      className="relative aspect-square max-h-52 w-full"
+      className="relative aspect-square h-full max-h-52 min-h-[150px] w-full"
       onClick={() => {
         onClick();
       }}
