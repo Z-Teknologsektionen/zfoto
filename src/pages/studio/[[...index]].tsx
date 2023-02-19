aa89b93c423ca69a564a215a2612e967af47dcbd
Dennis Holmström <dennis@holmstrom.nu>
2023-02-19 21:30:33 +0100

    Fix: Weird imports in sanity studio

--- src/pages/studio/[[...index]].tsx
+++ src/pages/studio/[[...index]].tsx
@@ -1,7 +1,6 @@
-/* eslint-disable import/no-unresolved */
 import type { NextPage } from "next";
-import { NextStudio } from "next-sanity/studio";
-import { NextStudioHead } from "next-sanity/studio/head";
+import { NextStudio } from "next-sanity/dist/studio";
+import { NextStudioHead } from "next-sanity/dist/studio/head";
 import Head from "next/head";
 
 import config from "../../../sanity.config";
