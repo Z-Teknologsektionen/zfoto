868f38a9222282b52e4c2d232bc717de1fe05b0e
Dennis Holmström <dennis@holmstrom.nu>
2023-07-22 16:47:56 +0200

    Fix: Auth error

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,8 +1,9 @@
 import { getServerSession } from "next-auth";
 import type { ReactNode } from "react";
+import authOptions from "~/server/auth";
 
 const AdminLayout = async ({ children }:{ children: ReactNode }) => {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
 
   if (!session?.user.email) {
     return <h1>Glöm och dröm</h1>; // TODO: add a custom unauthenticated screen
