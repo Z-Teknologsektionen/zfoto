40fa6bbf5e298dac2e34d4997a3dfbfed159df60
Dennis Holmström <dennis@holmstrom.nu>
2023-07-24 21:33:09 +0200

    Feat: added sidebar nav

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,8 +1,9 @@
 import { Prisma } from "@prisma/client";
-import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import { prisma } from "~/server/db/client";
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
+import InfoCard from "./info-card";
+import Sidebar from "./sidebar";
 
 const getCountsPerPhotographer = async () => {
   const images = await prisma.image.groupBy({
@@ -46,7 +47,7 @@ const getCountsPerPhotographer = async () => {
       coverImage: image._count.coverImage,
       album: album.filter((a) => a.images.some(samePhotographer)).length,
       firstImage: imagesDates.find(samePhotographer)?.date,
-      latestImage: imagesDates.reverse().find(samePhotographer)?.date,
+      latestImage: imagesDates.findLast(samePhotographer)?.date,
     };
   });
 
@@ -65,6 +66,22 @@ const getImageCountFromActive = async () => {
       date: {
         gt: new Date(activeYear, 10, 1),
         lt: new Date(activeYear + 1, 9, 31),
+      },
+    },
+  });
+};
+const getImageCountFromLastActive = async () => {
+  const currentDate = new Date();
+  let activeYear = currentDate.getFullYear();
+  if (currentDate.getMonth() < 10) {
+    activeYear -= 1;
+  }
+
+  return prisma.image.count({
+    where: {
+      date: {
+        gt: new Date(activeYear - 1, 10, 1),
+        lt: new Date(activeYear - 1 + 1, 9, 31),
       },
     },
   });
@@ -101,70 +118,60 @@ export type CountsPerPhotographerType = Prisma.PromiseReturnType<
 const page = async () => {
   const result = await getCountsPerPhotographer();
   const imagesThisYear = await getImageCountFromActive();
+  const imagesPrevYear = await getImageCountFromLastActive();
   const albumsThisYear = await getAlbumCountFromActive();
-  const totaltImages = await getTotalImageCount();
-  const totaltAlbums = await getTotalAlbumCount();
+  const totalImages = await getTotalImageCount();
+  const totalAlbums = await getTotalAlbumCount();
   const numberOfPhotographers = result.length;
+
+  const imagesPrevVSThisYear = `${
+    imagesThisYear - imagesPrevYear > 0 ? "+" : "-"
+  }${Math.abs(imagesThisYear - imagesPrevYear)}`;
 
   return (
     <>
-      {/* TODO: Add sidebar with links */}
-      <section className="container grid gap-x-2 gap-y-4 sm:grid-cols-2 lg:grid-cols-4">
-        <h1 className="col-span-full text-xl font-semibold">Statistik</h1>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Antal bilder</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">{totaltImages}</div>
-            <p className="text-muted-foreground text-xs">
-              +20.1% from last month
-            </p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Antal album</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">{totaltAlbums}</div>
-            <p className="text-muted-foreground text-xs">
-              {((albumsThisYear / totaltAlbums) * 100).toFixed(2)}% av sittande
-            </p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">
-              Antal fotografer
-            </CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">{numberOfPhotographers}</div>
-            <p className="text-muted-foreground text-xs">
-              +20.1% from last month
-            </p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">
-              Bilder av sittande
-            </CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">{imagesThisYear}</div>
-            <p className="text-muted-foreground text-xs">
-              {((imagesThisYear / totaltImages) * 100).toFixed(2)}% av totalt
-              antal bilder
-            </p>
-          </CardContent>
-        </Card>
-      </section>
-      <section className="container">
-        <h1 className="text-xl font-semibold">Fotografer</h1>
-        <DataTable columns={columns} data={result} />
-      </section>
+      <div className="flex flex-col gap-4 lg:flex-row">
+        <h1 className="text-center text-3xl font-bold lg:hidden">
+          Admin panel
+        </h1>
+        <Sidebar />
+        <div className="space-y-8">
+          <h1 className="container hidden text-3xl font-bold lg:block">
+            Admin panel
+          </h1>
+          <section className="container grid gap-x-2 gap-y-4 sm:grid-cols-2 lg:grid-cols-4">
+            <h2 className="col-span-full text-xl font-semibold">Statistik</h2>
+            <InfoCard
+              title="Antal bilder"
+              number={totalImages}
+              info={`${((imagesThisYear / totalImages) * 100).toFixed(2)}% av
+              sittande`}
+            />
+            <InfoCard
+              title="Antal album"
+              number={totalAlbums}
+              info={`${((albumsThisYear / totalAlbums) * 100).toFixed(2)}% av
+                  sittande`}
+            />
+            <InfoCard
+              title="Antal fotografer"
+              number={numberOfPhotographers}
+              info={`~${(totalImages / totalAlbums).toFixed(
+                2
+              )} bilder per album`}
+            />
+            <InfoCard
+              title="Bilder av sittande"
+              number={imagesThisYear}
+              info={`${imagesPrevVSThisYear} mot förra sittande`}
+            />
+          </section>
+          <section className="container">
+            <h2 className="text-xl font-semibold">Fotografer</h2>
+            <DataTable columns={columns} data={result} />
+          </section>
+        </div>
+      </div>
     </>
   );
 };
