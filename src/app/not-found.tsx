30d515c2393a7f5fe05068191200651ff67d92ba
Dennis Holmström <dennis@holmstrom.nu>
2023-07-28 21:58:11 +0200

    Feat: moved 404 page to app router

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,12 @@
 import type { NextPage } from "next";
-import MainLayout from "~/components/layout/MainLayout";
 
 const Custom404Page: NextPage = () => {
   return (
-    <MainLayout>
+    <>
       <div className="grid h-full flex-grow place-items-center text-3xl">
         <h1>Sidan du letar efter finns inte!</h1>
       </div>
-    </MainLayout>
+    </>
   );
 };
 
