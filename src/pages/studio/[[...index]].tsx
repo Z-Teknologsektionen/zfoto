7990025cf93d38c16304a42a96b74b9c85930553
Dennis Holmström <85495097+DHolmstrom@users.noreply.github.com>
2024-01-10 14:45:01 +0100

    Chore: Update packages

--- src/pages/studio/[[...index]].tsx
+++ src/pages/studio/[[...index]].tsx
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-unresolved */
 import type { NextPage } from "next";
 import { NextStudio } from "next-sanity/studio";
-import { NextStudioHead } from "next-sanity/studio/head";
+import { metadata } from 'next-sanity/studio/metadata';
 import Head from "next/head";
 
 import config from "../../../sanity.config";
@@ -10,7 +10,9 @@ const StudioPage: NextPage = () => {
   return (
     <>
       <Head>
-        <NextStudioHead />
+        {Object.entries(metadata).map(([key, value]) => (
+          <meta key={key} name={key} content={value} />
+        ))}
       </Head>
       <NextStudio config={config} />
     </>
