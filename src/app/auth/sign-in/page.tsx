41dc047ff15a6bee43c8a85bafcb0791fd8d2419
Dennis Holmström <85495097+DHolmstrom@users.noreply.github.com>
2024-05-14 09:19:57 +0200

    refactor(signin): Move to smaller more reusable components with single responsibility

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,26 +1,24 @@
 import { Metadata } from "next";
 import { FC } from "react";
 import SectionWrapper from "~/components/layout/SectionWrapper";
-import SignInContent from "./sign-in-content";
+import { SignInContent } from "./components/sign-in-content";
 
 export const metadata: Metadata = {
   title: "Logga in",
 };
 
-const LoginPage: FC = async () => {
+const LoginPage: FC = () => {
   return (
-    <div className="grid w-full flex-grow place-items-center">
-      <SectionWrapper className="max-w-96">
-        <h1 className="text-center text-3xl font-semibold leading-none tracking-tight">
-          Logga in
-        </h1>
-        <p className="text-xs text-neutral-500 dark:text-neutral-400">
-          Du som aktiv i Z-Teknologsektionen kan logga in med ditt Google-konto.
-          Om du har fått ett eget inlogg kan du logga in med din epost.
-        </p>
-        <SignInContent />
-      </SectionWrapper>
-    </div>
+    <SectionWrapper className="flex w-full max-w-96 flex-grow flex-col justify-center gap-4 space-y-0">
+      <h1 className="text-center text-3xl font-semibold leading-none tracking-tight">
+        Logga in
+      </h1>
+      <p className="text-xs text-neutral-500">
+        Du som aktiv i Z-Teknologsektionen kan logga in med ditt Google-konto.
+        Om du har fått ett eget inlogg kan du logga in med din epost.
+      </p>
+      <SignInContent />
+    </SectionWrapper>
   );
 };
 
