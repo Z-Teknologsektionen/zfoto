7ff0a935ba0c599bcbb9e1a040984a43a3f25732
Dennis Holmström <85495097+DHolmstrom@users.noreply.github.com>
2024-05-27 18:47:37 +0200

    fix(global): Rewrite date utils to work as expected

--- src/app/_utils/date-utils.ts
+++ src/app/_utils/date-utils.ts
@@ -1,15 +1,26 @@
 import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
 
+export const UTC_TIMEZONE = "utc";
+export const SWE_TIMEZONE = "Europe/Stockholm";
+
 dayjs.extend(utc);
+dayjs.extend(timezone);
+
+dayjs.tz.setDefault(SWE_TIMEZONE);
+dayjs.locale(SWE_TIMEZONE);
 
 export const formatDateString = (date: Date): string =>
   dayjs(date).format("YYYY-MM-DD");
 
 export const formatDateTimeString = (date: Date): string =>
-  dayjs(date).format("YYYY-MM-DD HH:MM:DD");
+  dayjs(date).format("YYYY-MM-DD HH:mm:ss");
 
 export const getLocalDateTimeFromUTC = (UTCDate: Date) =>
-  dayjs.utc(UTCDate).local();
+  dayjs(UTCDate).tz(UTC_TIMEZONE, true).toDate();
 
-export const getUTCFromLocalDate = (localDate: Date) => dayjs(localDate).utc();
+export const getUTCFromLocalDate = (localDate: Date) =>
+  dayjs(localDate.toISOString().slice(0, -8)).tz(UTC_TIMEZONE).toDate();
+
+export { dayjs as dayjsSWE };
