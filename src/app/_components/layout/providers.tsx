6dc5f76f4fe9dcceb9372e8fa7fb09d3c9002e17
Dennis Holmström <85495097+DHolmstrom@users.noreply.github.com>
2024-07-29 20:33:08 +0200

    feat: Remove TRPC in favor of next-safe-action

--- src/app/_components/layout/providers.tsx
+++ src/app/_components/layout/providers.tsx
@@ -1,40 +1,8 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { httpBatchLink, loggerLink } from "@trpc/react-query";
 import { SessionProvider } from "next-auth/react";
 import type { FC, PropsWithChildren } from "react";
-import { useMemo } from "react";
-import superjson from "superjson";
-import { trpc } from "~/trpc/client";
-import { getBaseUrl } from "~/utils/utils";
 
-export const Providers: FC<PropsWithChildren> = ({ children }) => {
-  const queryClient = useMemo(() => new QueryClient(), []);
-  const trpcClient = useMemo(
-    () =>
-      trpc.createClient({
-        links: [
-          loggerLink({
-            enabled: (opts) =>
-              process.env.NODE_ENV === "development" ||
-              (opts.direction === "down" && opts.result instanceof Error),
-          }),
-          httpBatchLink({
-            url: `${getBaseUrl()}/api/trpc`,
-          }),
-        ],
-        transformer: superjson,
-      }),
-    [],
-  );
-  return (
-    <SessionProvider>
-      <trpc.Provider client={trpcClient} queryClient={queryClient}>
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
-      </trpc.Provider>
-    </SessionProvider>
-  );
-};
+export const Providers: FC<PropsWithChildren> = ({ children }) => (
+  <SessionProvider>{children}</SessionProvider>
+);
