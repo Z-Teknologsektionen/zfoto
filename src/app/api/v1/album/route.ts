6675252b558ba2cf606d25490b2df505eeac49c8
Dennis Holmström <85495097+DHolmstrom@users.noreply.github.com>
2024-11-17 18:30:41 +0100

    feat: Move api routes to app router and remove unused API routes

---
+++ src/app/api/v1/album/route.ts
@@ -0,0 +1,40 @@
+import { createAlbumAPISchema } from "@/schemas/album";
+import { upsertAlbum } from "@/server/data-access/albums";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+
+export const POST = async (request: Request): Promise<Response> => {
+  try {
+    const parse = createAlbumAPISchema.safeParse(request.body);
+
+    if (!parse.success) {
+      return Response.json(
+        { error: parse.error.flatten().fieldErrors },
+        {
+          status: 400,
+        },
+      );
+    }
+
+    const createdAlbum = await upsertAlbum(parse.data);
+
+    return Response.json(createdAlbum, { status: 200 });
+  } catch (err) {
+    if (err instanceof PrismaClientKnownRequestError) {
+      if (err.code === "P2002") {
+        return Response.json(
+          {
+            error:
+              "Image with filename already exists, please check filenames and try again. No album has been created",
+          },
+          {
+            status: 400,
+          },
+        );
+      }
+    }
+    return Response.json(
+      { error: "Unknown error, try again later" },
+      { status: 500 },
+    );
+  }
+};
