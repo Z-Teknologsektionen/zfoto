a77929d4762955adcf691174c1f3eda0f52a4df4
Dennis Holmström <dennis@holmstrom.nu>
2025-12-14 12:58:12 +0100

    revert: import order matters for prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,8 @@
-import { defineConfig, env } from "prisma/config";
+//dotenv must be the first thing to be imported
 import "dotenv/config";
+
+// eslint-disable-next-line perfectionist/sort-imports
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
